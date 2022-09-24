a0854a604b805e7d1029f3579073a5c56ae12112
muhammadhafizm <muhammad.hafiz92@ui.ac.id>
2022-09-24 14:20:32 +0700

    feat: landing page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  fonts: {
+    body: `'Roboto', sans-serif`,
+  },
+  color: {
+    marlboro: "#B73A38",
+    mild: "#FFFFFF",
+    apple: "#ED1C24",
+    rose: "#95302E",
+    black: "black"
+  }
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
