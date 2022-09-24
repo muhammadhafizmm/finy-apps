f974d320fd182971557b23cd871448f75b999720
muhammadhafizm <muhammad.hafiz92@ui.ac.id>
2022-09-24 20:20:05 +0700

    convert to mobile first

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,11 @@
-import { Box, Flex, Image, Text, Button, Link } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Image,
+  Text,
+  Button,
+  useMediaQuery,
+} from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { theme } from "../pages/_app";
 
@@ -6,47 +13,57 @@ const NAVBAR_DESKTOP_LIST_WIDTH = 520;
 
 export default function Navbar() {
   const router = useRouter();
+  const [isMobile] = useMediaQuery("(max-width: 768px)");
   return (
     <Flex
+      position={isMobile ? "relative" : "absolute"}
+      left="0"
       justifyContent={"space-between"}
       backgroundColor={theme.color.mild}
       w="100%"
-      padding={"1em 3em"}
+      padding={isMobile ? "1em 1.5em" : "1em 3em"}
       boxShadow={"0px 10px 5px rgba(0, 0, 0, 0.08)"}
     >
       <Box>
-        <Image src="https://ik.imagekit.io/znmtfjgtk/Finy/logo-red-black_FYGp0atON.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1663993532635" />
+        <Image
+        src="https://ik.imagekit.io/znmtfjgtk/Finy/logo-red-black_FYGp0atON.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1663993532635" />
       </Box>
       <Flex
         flexDirection={"row"}
         alignItems={"center"}
         w={NAVBAR_DESKTOP_LIST_WIDTH}
-        justifyContent={"space-between"}
+        justifyContent={isMobile ? "flex-end" : "space-between"}
       >
-        <Text fontWeight={"medium"}>Produk</Text>
-        <Text fontWeight={"medium"}>Tentang Kami</Text>
-        <Flex flexDirection={"row"} alignItems="center">
-          <Text fontWeight={theme.color.black} color={theme.color.apple}>
-            ID
-          </Text>
-          <Box paddingX={".7em"}>
-            <Box
-              height="14px"
-              backgroundColor={theme.color.black}
-              width="1px"
-            />
-          </Box>
-          <Text fontWeight={"medium"}>EN</Text>
-        </Flex>
+        {!isMobile ? (
+          <>
+            <Text fontWeight={"medium"}>Produk</Text>
+            <Text fontWeight={"medium"}>Tentang Kami</Text>
+            <Flex flexDirection={"row"} alignItems="center">
+              <Text fontWeight={theme.color.black} color={theme.color.apple}>
+                ID
+              </Text>
+              <Box paddingX={".7em"}>
+                <Box
+                  height="14px"
+                  backgroundColor={theme.color.black}
+                  width="1px"
+                />
+              </Box>
+              <Text fontWeight={"medium"}>EN</Text>
+            </Flex>{" "}
+          </>
+        ) : null}
+
         <Button
+          size={isMobile ? "sm" : "md"}
           onClick={() => router.push("/login")}
           borderRadius={"10px"}
-          background={theme.color.apple}
+          background={theme.color.marlboro}
           color={theme.color.mild}
           border="solid 2px transparent"
           _hover={{
-            border: `solid 2px ${theme.color.apple}`,
-            color: theme.color.apple,
+            border: `solid 2px ${theme.color.marlboro}`,
+            color: theme.color.marlboro,
             bg: theme.color.mild,
           }}
         >
