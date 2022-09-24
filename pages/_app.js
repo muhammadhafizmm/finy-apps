9d2704e7469ca492cf1bded7996760666ca38b5b
muhammadhafizm <muhammad.hafiz92@ui.ac.id>
2022-09-24 22:31:42 +0700

    install fcm

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import "../styles/globals.css";
-import { Flex, Box, ChakraProvider } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { Box, ChakraProvider, useToast } from "@chakra-ui/react";
 import { useMediaQuery } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/react";
+import * as firebase from "firebase/app";
+import { firebaseCloudMessaging } from "../utils/firebase";
 
 export const theme = extendTheme({
   fonts: {
@@ -16,8 +18,7 @@ export const theme = extendTheme({
     black: "#000000",
   },
   breakpoints: {
-    xs: "360",
-    sm: "520px",
+    sm: "320px",
     md: "768px",
     lg: "960px",
     xl: "1200px",
@@ -26,14 +27,54 @@ export const theme = extendTheme({
 
 function MyApp({ Component, pageProps }) {
   const [isMobile] = useMediaQuery("(max-width: 768px)");
+  const toast = useToast();
+
+  useEffect(() => {
+    setToken();
+
+    // Event listener that listens for the push notification event in the background
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.addEventListener("message", (event) => {
+        console.log("event for the service worker", event);
+      });
+    }
+
+    // Calls the getMessage() function if the token is there
+    async function setToken() {
+      try {
+        const token = await firebaseCloudMessaging.init();
+        if (token) {
+          console.log("token", token);
+          getMessage();
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  });
+
+  // Get the push notification message and triggers a toast to display it
+  function getMessage() {
+    const messaging = firebase.messaging();
+    messaging.onMessage((message) => {
+      toast({
+        title: "Test.",
+        description: "Mantap ga tuh.",
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+      });
+    });
+  }
+
   return (
     <ChakraProvider theme={theme}>
       <Box
         width={"100vw"}
         style={
           isMobile
-          ? { display: "block" }
-          : { display: "flex", justifyContent: "center" }
+            ? { display: "block" }
+            : { display: "flex", justifyContent: "center" }
         }
       >
         <Box style={isMobile ? { maxWidth: "100%" } : { width: 768 }}>
