ba602b450d339c3a6ea5bb0bebbe8266d1a2f266
muhammadhafizm <muhammad.hafiz92@ui.ac.id>
2022-09-25 02:19:54 +0700

    login sukses

--- pages/login-success.js
+++ pages/login-success.js
@@ -1,14 +1,77 @@
-import { Text } from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import {
+  Flex,
+  VStack,
+  keyframes,
+  Text,
+  Box,
+  usePrefersReducedMotion,
+} from "@chakra-ui/react";
+import Notification from "../components/Notification";
+import { theme } from "./_app";
 
-export default function LoginSuccess() {
+export default function LoginCheck() {
   const router = useRouter();
+  const [height, setHeight] = useState("100vh");
+
   useEffect(() => {
     setTimeout(() => {
       window.location.replace("/")
-    }, 3000);
+    }, 3500);
   }, []);
 
-  return <Text>Login Berhasil</Text>;
+  useEffect(() => {
+    setHeight(window.innerHeight + "px");
+  });
+
+  const prefersReducedMotion = usePrefersReducedMotion();
+  const animation1 = prefersReducedMotion
+    ? undefined
+    : `${keyframe_notif1} 3s ease`;
+
+  return (
+    <Flex
+      position={"relative"}
+      h={height}
+      flexDirection={"column"}
+      alignItems={"center"}
+      padding="2em 0"
+      bg={theme.color.apple}
+      justifyContent="center"
+    >
+      <Box pos="absolute" top="3em">
+        <Box position={"relative"} animation={animation1}>
+          <Notification
+            description={"🎉Notifikasi berhasil di buat 🎉"}
+          ></Notification>
+        </Box>
+      </Box>
+      <Flex paddingTop={"3em"} justifySelf="center">
+        <Text
+          fontWeight={"bold"}
+          color={theme.color.mild}
+          fontSize="3xl"
+          marginRight={"6px"}
+        >
+          Berhasil
+        </Text>
+      </Flex>
+    </Flex>
+  );
 }
+
+const keyframe_notif1 = keyframes`
+  0% {
+    bottom: -10px;
+    opacity: 0;
+  }
+  30% {
+    bottom: 0;
+    opacity: 1;
+  }
+  100% {
+    bottom: 0;
+    opacity: 1;
+  }
+`;
